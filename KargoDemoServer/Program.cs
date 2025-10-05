733f95514e677b9f61bda5ac77df6adf47dfe101
Evgeny Snitko <evgeny@parity.io>
2025-10-06 02:12:21 +0400

    Add project files.

---
+++ KargoDemoServer/Program.cs
@@ -0,0 +1,37 @@
+namespace KargoDemoServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllPolicy",
+                    b =>
+                    {
+                        b.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+            app.UseCors("AllowAllPolicy");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
